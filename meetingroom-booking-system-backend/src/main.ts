5f5cd27fe0b5230e9f3060737532613891cf9798
jiapandong <jiapandong@supconit.com>
2023-08-16 10:58:49 +0800

    feat: 用户管理模块生成 swagger 接口文档

--- meetingroom-booking-system-backend/src/main.ts
+++ meetingroom-booking-system-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
@@ -17,6 +18,19 @@ async function bootstrap() {
   // app.useGlobalFilters(new UnloginFilter())
 
   app.useGlobalFilters(new CustomExceptionFilter())
+
+  const config = new DocumentBuilder()
+    .setTitle('会议室预订系统')
+    .setDescription('api 接口文档')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      description: '基于 jwt 的认证'
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // 用 SwaggerModule 生成接口文档，url 是 /api-doc
+  SwaggerModule.setup('api-doc', app, document);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
